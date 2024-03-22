a7648672c64654df11dced3b597ede17077ee6d9
denfasyah <denfasyah@gmail.com>
2024-03-23 01:32:15 +0700

    add icon like,comment,share, bookmark

--- resources/views/fresh/index.blade.php
+++ resources/views/fresh/index.blade.php
@@ -2,6 +2,7 @@
 
 <div class="container">
   <div class="flex flex-wrap mt-6 -mx-3 scroll-behavior">
+
     <div
       class="flex justify-between items-center w-full pb-5 px-3 mt-5 lg:mt-0 mb-6 lg:mb-0 lg:flex-none"
       id="youranswer"
@@ -85,7 +86,7 @@
         <!-- this hidden checkbox controls the state -->
         <input type="checkbox" />
 
-        <!-- volume on icon -->
+        <!-- asscending on icon -->
         <span class="bg-Blue rounded swap-on">
           <img
             src="{{ asset('icons/ascending.png') }}"
@@ -94,7 +95,7 @@
           />
         </span>
         
-        <!-- volume off icon -->
+        <!-- descending off icon -->
         <span class="bg-Blue rounded swap-off">
         <img
         src="{{ asset('icons/descending.png') }}"
@@ -104,34 +105,70 @@
       </label>
     </span>
     </div>
-    <!-- quest -->
+
+
+
+    <!-- Postingan -->
     <div class="w-full pb-5 px-3 mt-5 lg:mt-0 mb-6 lg:mb-0 lg:flex-none">
       <div
         class="flex flex-col min-w-0 break-words bg-white dark:bg-slate-850 shadow-soft-xl rounded-2xl bg-clip-border"
       >
         <div class="flex-auto px-4 pb-4">
           <div class="flex flex-wrap -mx-3">
-            <div class="max-w-full px-3 lg:w-1/2 lg:flex-none">
+            <div class="w-full px-3">
               <div class="flex flex-col h-full">
                 <div class="avatar flex items-center">
                   <div class="mask mask-circle w-10 lg:w-14 md:w-14 sm:w-8">
                     <img src="{{ asset('icons/anime.png') }}" />
                   </div>
                   <div class="flex flex-col gap-2">
+
+                    <div class="absolute right-0 mt-5">
+                      <img src="{{ asset('icons/bookmarkicon.svg') }}" alt="">
+                    </div>
+
                     <h5
                       class="font-bold mt-4 ml-5 h-4 text-black dark:text-white"
                     >
                       Adent
                     </h5>
+                    
                     <span class="text-xs ml-5 text-black dark:text-white"
                       >10.00</span
                     >
+
                   </div>
                 </div>
                 <p class="mb-1 font-semibold text-black dark:text-white">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Similique, laudantium?
                 </p>
+
+                <div class="flex w-full gap-3 mt-5">
+                  <!-- icon like -->
+                  <div class="flex items-center gap-1 h-10 bg-white dark:bg-slate-850 rounded-2xl">
+                  <img src="{{ asset('icons/likeicon.svg') }}" alt="">
+                  <p class="mt-4 mr-2 text-xs font-bold font-inter text-slate-500 dark:text-white">1M </p>
+                  </div>
+
+                  <!-- icon comment -->
+                  <div class="flex items-center gap-1 h-10 bg-white dark:bg-slate-850 rounded-2xl">
+                    <img src="{{ asset('icons/commenticon.svg') }}" alt="">
+                    <p class="mt-4 mr-2 text-xs font-bold font-inter text-slate-500 dark:text-white">12 </p>
+                  </div>
+
+                  <!-- icon share -->
+                  <div class="flex items-center gap-1 h-10 bg-white dark:bg-slate-850 rounded-2xl">
+                    <img src="{{ asset('icons/shareicon.svg') }}" alt="">
+                    <p class="mt-4 mr-2 text-xs font-bold font-inter text-slate-500 dark:text-white">12 </p>
+                  </div>
+
+                  <div class="bg-Blue text-white text-xs px-2 mt-2.5 h-5 rounded-xl absolute right-10">
+                    <p>Anime</p>
+                  </div>
+                  
+                </div>
+
               </div>
             </div>
           </div>
